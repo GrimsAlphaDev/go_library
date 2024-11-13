998ff3dc836efd2c167f83ee209161e0802c1eb3
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-11-13 22:37:01 +0700

    intial commit

---
+++ fmt.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "Arthur"
+	lastName := "Morgan"
+
+	fmt.Println("Hello", firstName, lastName)
+	fmt.Printf("Hello '%s %s'!\n", firstName, lastName)
+}
